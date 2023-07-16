c7bd6f805f2ee9f3541e69b25888f4659e5414df
Ruslan Aliyev <ruslanra@code.edu.az>
2023-07-17 00:25:56 +0400

    first commit

---
+++ NumbAndSqr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbAndSqr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 9, 8, 6, 5 , 4 ,3, 2, 1 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int b = numbers[i];
+                int a = numbers[i] * numbers[i];
+                Console.WriteLine("{" + "Number=" + b + "," + " "  + "SqrNo=" + a +"}" +" " );
+
+            }
+        }
+    }
+}
